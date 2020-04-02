74a0ee5b5272610e717852011ac600578aa3e95b
kevinrizki1019 <13518100@std.stei.itb.ac.id>
2020-04-02 10:12:42 +0700

    Add class CharacterCard

---
+++ src/main/java/com/avatarduel/cards/CharacterCard.java
@@ -0,0 +1,26 @@
+package com.avatarduel.cards;
+
+public class CharacterCard extends Card {
+    private int attack;
+    private int defense;
+    private int power;
+
+    public CharacterCard(int id, String name, String description, int attack, int defense, int power) {
+        super(id, name, description);
+        this.attack = attack;
+        this.defense = defense;
+        this.power = power;
+    }
+
+    public int getAttack() {
+        return this.attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public int getPower() {
+        return power;
+    }
+}
